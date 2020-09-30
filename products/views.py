b56763c0b3293c9f8b7b2d6046a19a3b3cd356ac
Jesus Lopez Roldan <jesuslr@hotmail.com>
2020-09-30 12:38:41 -0500

    Raw HTML Form

--- products/views.py
+++ products/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def products_detailed_view(request):
     obj = Product.objects.all()
     context = {'objects': obj}
     return render(request, 'products/detail.html', context)
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    
+    if form.is_valid():
+        form.save()
+
+    context = {'form': form}
+    return render(request, 'products/product_create.html', context)
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view
-from products.views import products_detailed_view
+from products.views import products_detailed_view, product_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('products/', products_detailed_view),
+    path('create/', product_create_view),    
 ]
